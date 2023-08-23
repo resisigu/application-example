64c5c2a3b758aa039f3b4eabdbeff348e93749ae
Duy Ank <duyanh980@gmail.com>
2023-08-23 03:38:38 +0000

    Update 4 files

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from 'react';
-import Test from './src/Test';
-import Publish from './src/Publish';
+import Test from './src/Test'; // used to display editor
+import Publish from './src/Publish'; //used to display output. this support SSR.
 const App: FC = () => {
-    return <Test />;
+    return <Publish />;
 };
 
 export default App;
--- src/Publish.tsx
+++ src/Publish.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import { Frame } from '@lidojs/screen';
 import { data } from '../data';
+import { getPageSize } from '@lidojs/core';
 
 const Publish = () => {
-    return <Frame width={1920} height={1080} data={data} />;
+    const size = getPageSize(data);
+    return (
+        <div
+            css={{
+                minWidth: '100vw',
+                minHeight: '100vh',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            <Frame width={size.width} height={size.height} data={data} />
+        </div>
+    );
 };
 
 export default Publish;
