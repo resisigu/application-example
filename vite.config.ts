30d295e1fb942b7c877471496a555700733c521c
Akira <duyanh980@gmail.com>
2025-06-27 18:36:24 +0700

    chore: upgrade build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,28 @@
-/// <reference types='vitest' />
-import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { ViteEjsPlugin } from 'vite-plugin-ejs';
+import { analyzer } from 'vite-bundle-analyzer';
 import EnvironmentPlugin from 'vite-plugin-environment';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+const isProd = process.env.NODE_ENV === 'production';
+const plugins = [react(), EnvironmentPlugin(['API_ENDPOINT', 'FONT_API_KEY'])];
+if (isProd) {
+  plugins.push(
+    analyzer({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
+  );
+}
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/editor',
-
+  plugins,
   server: {
     port: 4200,
-    host: 'localhost',
-  },
-
-  preview: {
-    port: 4300,
-    host: 'localhost',
   },
   build: {
-    sourcemap: true,
+    sourcemap: true, // Generate source maps for debugging
+    rollupOptions: {
+      treeshake: true,
+    },
   },
-
-  plugins: [
-    ViteEjsPlugin(),
-    react(),
-    viteTsConfigPaths({
-      root: '../../',
-    }),
-    EnvironmentPlugin('all'),
-    sentryVitePlugin({
-      org: 'liquid-editor',
-      project: 'liquid-editor-demo',
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-    }),
-  ],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
 });
